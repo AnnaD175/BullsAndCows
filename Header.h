ca21d9447a1ae706a91062bacd63d0f5e2f40940
Даниленко АС <annas.danilenko@yandex.ru>
2024-03-19 16:08:38 +0300

    ;

--- Header.h
+++ Header.h
@@ -1,4 +1,4 @@
 #pragma once
 
-void Random(int* secretNumber)
-void BullsAndCows(int* guessNumber, int* secretNumber, int* bulls, int* cows)
+void Random(int* secretNumber);
+void BullsAndCows(int* guessNumber, int* secretNumber, int* bulls, int* cows);
